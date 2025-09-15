4788852bfc8fcac648bc030aa3d8df75b8b663d6
ZuluMumu <saugatdahal030@gmail.com>
2025-09-15 19:58:24 +0545

    leetcode : partition, binary to dec, swap node in pairs

--- LinkedLists/leetcode.py
+++ LinkedLists/leetcode.py
@@ -1,7 +1,6 @@
 from linkedlist import LinkedList
-
-
-# find middle node:
+from linkedlist import Node
+# find middle node: (Floyd Cycle Determination)
 def find_middle(ll):
     slow = ll.head
     fast=ll.head
@@ -10,6 +9,7 @@ def find_middle(ll):
         fast = fast.next.next
     return slow.value
 
+#check for loop in the ll. (Floyd Cycle Determination)
 def has_loop(ll):
     slow = ll.head
     fast=ll.head
@@ -20,12 +20,73 @@ def has_loop(ll):
          return True
     return False
 
+#Finding the kth node from the end:
+def kth_node_from_end(ll, index):
+    if index <= 0 or index > ll.length:
+        print("Out of bounds error")
+        return None
+
+    slow = ll.head
+    fast = ll.head
+
+    for _ in range(index-1):
+        if fast is None:
+           return None
+        fast = fast.next
+
+    while fast.next is not None:
+        slow = slow.next
+        fast = fast.next
+
+    return slow.value
+
+# def remove_duplicated(ll):
+
+def binary_to_decimal(ll):
+ num = 0
+ current = ll.head
+ while current:
+    num = num * 2 + current.value
+    current = current.next
+ return num
+   
+
+
+#partition the list such that anything below "partition_value" is on one side and anything greater than or equal to "partition_value" is in the other side.
+def partition_list(ll, partition_value):
+   #My approach:
+   dummy_list1 = LinkedList(999)
+   dummy_list2 = LinkedList(111)
+   temp = ll.head
+   while temp is not None:
+      if temp.value < partition_value:
+         dummy_list1.append(temp.value)
+      else:
+         dummy_list2.append(temp.value)
+      temp = temp.next
+   
+   dummy_list1.tail.next = dummy_list2.head.next
+   ll.head = dummy_list1.head.next
+   #solution ko approach:
+   
+
+
+# def swap_node_in_pairs(ll):
+#    slow = ll.head
+#    fast = ll.head   
+
+
+
+
 
 my_linked_list = LinkedList(1)
 my_linked_list.append(2)
-my_linked_list.append(3)
-my_linked_list.append(4)
-my_linked_list.append(5)
+my_linked_list.append(3) #3
+my_linked_list.append(4) #2
+my_linked_list.append(5) #1
 # Find middle node
 # print("middle node:" , find_middle(my_linked_list))
-print("Has loop:", has_loop(my_linked_list))
+# Has loop:
+# print("Has loop:", has_loop(my_linked_list))
+# Kth node from tail:
+# print("Has 3rd from tail", kth_node_from_end(my_linked_list,3).value)
